f4953d27c497bb280ecd84fb6abe312fb1cf7150
esrafil elahi <esrafil.elahi@gmail.com>
2024-05-08 15:50:42 +0330

    add dynamic component

---
+++ dynamic-component.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <button @click="toggleComponent">Toggle Component</button>
+    <component :is="currentComponent"></component>
+  </div>
+</template>
+  
+<script>
+import { ref } from 'vue';
+import ComponentA from './components/ComponentA.vue';
+import ComponentB from './components/ComponentB.vue';
+
+export default {
+  setup() {
+    const currentComponent = ref('ComponentA');
+
+    const toggleComponent = () => {
+      currentComponent.value = currentComponent.value === 'ComponentA' ? 'ComponentB' : 'ComponentA';
+    };
+
+    return {
+      currentComponent,
+      toggleComponent
+    };
+  },
+  components: {
+    ComponentA,
+    ComponentB
+  }
+};
+</script>
+  
