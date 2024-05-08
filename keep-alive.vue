7a53443c3c54416c9d4f9c13e083a8eb70f38c21
esrafil elahi <esrafil.elahi@gmail.com>
2024-05-08 16:32:24 +0330

    add keep alive

---
+++ keep-alive.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { shallowRef } from 'vue'
+import CompA from './CompA.vue'
+import CompB from './CompB.vue'
+
+const current = shallowRef(CompA)
+</script>
+
+<template>
+  <div class="demo">
+    <label><input type="radio" v-model="current" :value="CompA" /> A</label>
+    <label><input type="radio" v-model="current" :value="CompB" /> B</label>
+    <KeepAlive>
+      <component :is="current"></component>
+    </KeepAlive>
+  </div>
+</template>
