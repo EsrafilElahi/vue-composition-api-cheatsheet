72f58a4748dac479fe7ebb431da1425a23834138
esrafil elahi <esrafil.elahi@gmail.com>
2024-05-08 15:45:23 +0330

    add custom-directive global

---
+++ custom-directive-global.vue
@@ -0,0 +1,31 @@
+// main.js
+import { createApp } from 'vue';
+import App from './App.vue';
+
+const app = createApp(App);
+
+// Custom directive
+app.directive('uppercase', {
+  mounted(el) {
+    el.addEventListener('input', function(event) {
+      event.target.value = event.target.value.toUpperCase();
+    });
+  }
+});
+
+app.mount('#app');
+
+
+<template>
+  <div>
+    <label for="inputField">Enter Text:</label>
+    <input type="text" v-uppercase id="inputField">
+  </div>
+</template>
+  
+<script>
+export default {
+  name: 'MyComponent'
+};
+</script>
+  
