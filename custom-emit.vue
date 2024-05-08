71f6b95838a44e91822aa079b229bbc487c890bb
esrafil elahi <esrafil.elahi@gmail.com>
2024-05-08 15:48:44 +0330

    custom emit

---
+++ custom-emit.vue
@@ -0,0 +1,30 @@
+<script>
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  setup(_, { emit }) {
+    const emitEvent = () => {
+      emit('custom-event', 'Hello from child component');
+    };
+
+    return {
+      emitEvent
+    };
+  }
+});
+</script>
+
+<!-- ==== usage ==== -->
+
+<script>
+import { onMounted } from 'vue';
+
+export default {
+  props: ['parentData'],
+  setup(props) {
+    onMounted(() => {
+      console.log('Parent data:', props.parentData);
+    });
+  }
+};
+</script>
