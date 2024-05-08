a53168eff8109f5fda64346dcbf5b768ccbb168f
esrafil elahi <esrafil.elahi@gmail.com>
2024-05-08 15:47:32 +0330

    add provider-inject

---
+++ provider-inject.vue
@@ -0,0 +1,25 @@
+<script>
+import { provide } from 'vue';
+
+export default {
+  setup() {
+    provide('sharedData', 'Hello from parent component');
+  }
+};
+</script>
+
+<!-- ==== usage ==== -->
+
+<script>
+import { inject, onMounted } from 'vue';
+
+export default {
+  setup() {
+    const parentData = inject('sharedData');
+
+    onMounted(() => {
+      console.log('Parent data:', parentData);
+    });
+  }
+};
+</script>
