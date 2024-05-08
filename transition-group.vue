9eed00ddc1842af14ad4e1bdf3ad55c70482f49a
esrafil elahi <esrafil.elahi@gmail.com>
2024-05-08 16:21:40 +0330

    add transition-group

---
+++ transition-group.vue
@@ -0,0 +1,18 @@
+<TransitionGroup name="list" tag="ul">
+    <li v-for="item in items" :key="item">
+      {{ item }}
+    </li>
+</TransitionGroup>
+
+<style scoped>
+.list-enter-active,
+.list-leave-active {
+    transition: all 0.5s ease;
+}
+
+.list-enter-from,
+.list-leave-to {
+    opacity: 0;
+    transform: translateX(30px);
+}
+</style>
