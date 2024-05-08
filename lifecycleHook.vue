91b3df861c90102a194ab978eba5fffae752d27c
esrafil elahi <esrafil.elahi@gmail.com>
2024-05-08 15:51:26 +0330

    add onMounted

--- lifecycleHook.vue
+++ lifecycleHook.vue
@@ -1,6 +1,6 @@
 <!-- watch -->
 <script>
-import { ref, watch } from 'vue';
+import { ref, watch, onMounted, onUnmounted } from 'vue';
 
 export default {
   setup() {
@@ -8,6 +8,14 @@ export default {
 
     watch(message, (newVal, oldVal) => {
       console.log('Message changed from', oldVal, 'to', newVal);
+    });
+
+    onMounted(() => {
+      console.log('Component mounted');
+    });
+
+    onUnmounted(() => {
+      console.log('Component unmounted');
     });
 
     return {
