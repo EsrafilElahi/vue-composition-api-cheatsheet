2fc3a969201b07d5afb7d2bf7ae6222ebd208f2e
esrafil elahi <esrafil.elahi@gmail.com>
2024-05-08 16:17:22 +0330

    add Transition

---
+++ transition.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue'
+
+const show = ref(true)
+</script>
+
+<template>
+  <button @click="show = !show">Toggle</button>
+  <Transition>
+    <p v-if="show">hello</p>
+  </Transition>
+</template>
+
+<style>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
