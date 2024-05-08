26b0f17f17cdebe6b823cb2040c0112ee8629a2e
esrafil elahi <esrafil.elahi@gmail.com>
2024-05-08 15:50:13 +0330

    add watch

---
+++ lifecycleHook.vue
@@ -0,0 +1,18 @@
+<!-- watch -->
+<script>
+import { ref, watch } from 'vue';
+
+export default {
+  setup() {
+    const message = ref('');
+
+    watch(message, (newVal, oldVal) => {
+      console.log('Message changed from', oldVal, 'to', newVal);
+    });
+
+    return {
+      message
+    };
+  }
+};
+</script>
